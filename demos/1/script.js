3cae9938c5fd2d94fb0317897e88794ddc044b98
2390806+lucidbard@users.noreply.github.com <2390806+lucidbard@users.noreply.github.com>
2024-06-05 17:36:14 +0000

    Jun 5, 2024, 10:36 AM

--- demos/1/script.js
+++ demos/1/script.js
@@ -1,9 +1,36 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight)
-  print("Hello world")
-}
-
-function draw() {
-  // Draw a circle in the center of the screen
-  circle(width / 2, height / 2, 50)
+  createCanvas(400, 400);
+  background(255);
+  noFill();
+  stroke(0);
+  
+  // Draw hand-drawn circle
+  beginShape();
+  vertex(100, 100);
+  vertex(110, 90);
+  vertex(120, 95);
+  vertex(130, 100);
+  vertex(140, 115);
+  vertex(130, 130);
+  vertex(120, 135);
+  vertex(110, 130);
+  vertex(100, 115);
+  endShape(CLOSE);
+  
+  // Draw hand-drawn square
+  beginShape();
+  vertex(200, 100);
+  vertex(250, 100);
+  vertex(255, 110);
+  vertex(250, 150);
+  vertex(200, 150);
+  vertex(195, 140);
+  endShape(CLOSE);
+  
+  // Draw hand-drawn triangle
+  beginShape();
+  vertex(150, 250);
+  vertex(200, 300);
+  vertex(100, 300);
+  endShape(CLOSE);
 }
